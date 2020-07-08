558e940c3a2a49073b940431c59b18e4b3dbff25
congenial-course-2234 <jakelozano94@gmail.com>
2020-07-08 04:19:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -29,6 +29,8 @@ def nyc_pigeon_organizer(data)
       if !final_array[name][key]
         final_array[name][key] = []
       end
-      final_array[name][key] << value2.to_s
-        
+      final_array[name][key] << key2.to_s
+    end
+  end
+end
 end
